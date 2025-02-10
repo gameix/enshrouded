cc4fccac6d5ac763672d1f0d31197592edf1648f
ASoels <albert.soels@frequentis.com>
2025-02-10 10:38:01 +0100

    Update enshrouded image 17

--- Docker/enshrouded/scripts/entrypoint.sh
+++ Docker/enshrouded/scripts/entrypoint.sh
@@ -23,5 +23,5 @@ function keepContainerRunning () {
 #########################################################
 #
 # Execute
-keepContainerRunning
+#keepContainerRunning
 enshrouded_dedicated_server
