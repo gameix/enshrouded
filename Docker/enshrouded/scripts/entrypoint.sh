33cdd5d48ce3a6a839c5a72a7207e0ffe2e83447
ASoels <albert.soels@frequentis.com>
2025-02-10 10:03:09 +0100

    Update base image 1

---
+++ Docker/enshrouded/scripts/entrypoint.sh
@@ -0,0 +1,14 @@
+##!/bin/bash
+
+
+
+# Keep Container running
+function keepContainerRunning () {
+  # Keep Alive
+  while :
+  do
+    sleeptime=10
+    echo "Keep Container running...Press [CTRL+C], next run in ${sleeptime} seconds";	sleep ${sleeptime}
+  done
+}
+
