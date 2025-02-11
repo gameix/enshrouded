70882c56ce13ead3a208f008196dbb39d63ae20d
albert <albert.soels@gameix.net>
2025-02-11 18:55:41 +0100

    Update Backup feature 7

--- Docker/enshrouded/scripts/backup.sh
+++ Docker/enshrouded/scripts/backup.sh
@@ -3,19 +3,21 @@
 # CMD
 CMD_CP="/usr/bin/cp"
 CMD_MKDIR="/usr/bin/mkdir"
+CMD_ECHO="/usr/bin/echo"
+CMD_DATE="/usr/bin/date"
 
 # Quick function to generate a timestamp
 timestamp () {
-  date +"%Y-%m-%d %H:%M:%S,%3N"
+  ${CMD_DATE} +"%Y-%m-%d %H:%M:%S,%3N"
 }
 
 
 function create_backup() {
-  echo "[$(timestamp)] -- BACKUP: Start Backup"
-  echo "[$(timestamp)] -- BACKUP: -> SOURCE: '${SOURCE}'"
-  echo "[$(timestamp)] -- BACKUP: -> TARGET: '${TARGET}'"
+  ${CMD_ECHO} "[$(timestamp)] -- BACKUP: Start Backup"
+  ${CMD_ECHO} "[$(timestamp)] -- BACKUP: -> SOURCE: '${SOURCE}'"
+  ${CMD_ECHO} "[$(timestamp)] -- BACKUP: -> TARGET: '${TARGET}'"
 
-  TARGET_DATE=$(date +"%Y-%m-%d_%H%M%S")
+  TARGET_DATE=$(${CMD_DATE} +"%Y-%m-%d_%H%M%S")
   ${CMD_MKDIR} -p ${TARGET}/${TARGET_DATE}
   ${CMD_CP} -a ${SOURCE}/* ${TARGET}/${TARGET_DATE}
   # excepteion handling if no files in SOURCE PATH
@@ -27,11 +29,11 @@ shift $((OPTIND - 1))
 SOURCE=$1
 TARGET=$2
 if [ -z "${SOURCE}" ]; then
-  echo "[$(timestamp)] -- BACKUP: ERROR no SOURCE Path given, exit script"
+  ${CMD_ECHO} "[$(timestamp)] -- BACKUP: ERROR no SOURCE Path given, exit script"
   exit 1
 fi
 if [ -z "${TARGET}" ]; then
-  echo "[$(timestamp)] -- BACKUP: ERROR no TARGET Path given, exit script"
+  ${CMD_ECHO} "[$(timestamp)] -- BACKUP: ERROR no TARGET Path given, exit script"
   exit 1
 fi
 
